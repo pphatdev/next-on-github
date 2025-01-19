e0d3f84d8515ba63ed97ece7d5d94d632c69b222
pphatdev <leatsophat02@gmail.com>
2025-01-19 21:34:18 +0700

    📦: Test Build

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,11 @@
-import Link from "next/link";
+import React from "react";
+import Hero from "./sections/hero";
 
 export default function Home() {
-  return (
-    <div>
-      Hello World. <Link href="/about">About</Link>
-    </div>
-  );
+    return (
+        <div className="w-full mx-auto min-h-screen overflow-y-auto">
+            <Hero />
+            {/* <Collision /> */}
+        </div>
+    );
 }
